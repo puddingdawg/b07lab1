b369e3c0b84db5727c8381a11711e3542d550380
William Choi <rivz@Williams-MacBook-Pro.local>
2023-06-12 00:22:05 -0400

    modified Polynomial.java and Driver.java

--- Driver.java
+++ Driver.java
@@ -1,16 +1,69 @@
+import java.io.File;
+
 public class Driver {
-    public static void main(String [] args) {
-        Polynomial p = new Polynomial(); 
+    public static void main(String[] args) {
+        Polynomial p = new Polynomial();
         System.out.println(p.evaluate(3));
-        double [] c1 = {6,0,0,5};
-        Polynomial p1 = new Polynomial(c1);
-        double [] c2 = {0,-2,0,0,-9};
-        Polynomial p2 = new Polynomial(c2); 
-        Polynomial s = p1.add(p2); 
-        System.out.println("s(0.1) = " + s.evaluate(0.1)); 
-        if(s.hasRoot(1))
+        double[] c1 = { 8, 9 };
+        int[] e1 = { 3, 2 };
+        Polynomial p1 = new Polynomial(c1, e1);
+        double[] c2 = { 1, 2, 3 };
+        int[] e2 = { 1, 2, 3 };
+        Polynomial p2 = new Polynomial(c2, e2);
+        Polynomial s = p1.add(p2);
+        System.out.println("s(0.1) = " + s.evaluate(0.1));
+        if (s.hasRoot(1))
             System.out.println("1 is a root of s");
         else
             System.out.println("1 is not a root of s");
+
+        // Multiply tester
+        System.out.print("Coeffi after multiplication: ");
+        Polynomial d = p1.multiply(p2);
+        for (int i = 0; i < d.expo.length; i++) {
+            System.out.print(d.coeffi[i] + " ");
+        }
+
+        System.out.println();
+
+        System.out.print("Expo after multiplication: ");
+        for (int i = 0; i < d.expo.length; i++) {
+            System.out.print(d.expo[i] + " ");
+        }
+
+        System.out.println();
+
+        // testing constructor
+        try {
+            File file = new File("/Users/rivz/b07lab1/lab2.txt");
+            Polynomial z = new Polynomial(file);
+            System.out.print("Coefficient from file: ");
+            for (int i = 0; i < z.coeffi.length; i++) {
+                System.out.print(z.coeffi[i] + " ");
+            }
+            System.out.println();
+
+            System.out.print("Expo from file: ");
+            for (int i = 0; i < z.expo.length; i++) {
+                System.out.print(z.expo[i] + " ");
+            }
+
+            System.out.println();
+
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+
+        }
+
+        try {
+            // testing Save to File
+            String filepath = "/Users/rivz/b07lab1/lab2save.txt";
+            Polynomial t = new Polynomial(c2, e2);
+            t.saveToFile(filepath);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+
+        }
+
     }
 }
--- Polynomial.java
+++ Polynomial.java
@@ -1,54 +1,205 @@
+import java.io.File;
+import java.util.Scanner;
+import java.io.FileWriter;
+import java.io.FileNotFoundException;
+import java.util.Arrays;
+
 public class Polynomial {
-    double[] coeffi;
+    public double[] coeffi;
+    public int[] expo;
 
-    public Polynomial(){
+    public Polynomial() {
         this.coeffi = new double[0];
+        this.expo = new int[0];
     }
 
-    public Polynomial(double [] coeffi){
-            this.coeffi = coeffi;
+    public Polynomial(double[] coeffi, int[] expo) {
+        this.coeffi = coeffi;
+        this.expo = expo;
     }
 
-    //Add method
-    public Polynomial add(Polynomial poly){
-        int length = poly.coeffi.length;
+    // Add method
+    public Polynomial add(Polynomial poly) {
+        int elength = poly.expo.length;
 
-        if(this.coeffi.length > length){
-            length = this.coeffi.length;
+        int maxLen = this.expo.length + elength;
+        int[] temp = new int[maxLen];
+        int count = 0;
+
+        for (int i = 0; i < maxLen; i++) {
+            temp[i] = -1;
         }
 
-        double[] newPoly = new double[length];
-
-        for(int i = 0; i<this.coeffi.length; i++)
-        {
-            newPoly[i] = this.coeffi[i];
+        for (int i = 0; i < this.expo.length; i++) {
+            if (!this.dupChecker(this.expo[i], temp)) {
+                temp[count] = this.expo[i];
+                count += 1;
+            }
         }
 
-        for(int i = 0; i<poly.coeffi.length; i++)
-        {
-            newPoly[i] += poly.coeffi[i];
+        for (int i = 0; i < poly.expo.length; i++) {
+            if (!this.dupChecker(poly.expo[i], temp)) {
+                temp[count] = poly.expo[i];
+                count += 1;
+            }
         }
 
-        return new Polynomial(newPoly);
-    }    
+        // create exponent array
+        int[] totalexpo = new int[count];
+        for (int i = 0; i < count; i++) {
+            totalexpo[i] = temp[i];
+        }
 
-    //Evaluate method
-    public double evaluate(double x){
+        // sort exponent
+        Arrays.sort(totalexpo);
+
+        // Adding Pt 1
+        double[] coeffiFinal = new double[count];
+        for (int i = 0; i < this.expo.length; i++) {
+            int ti = indexChecker(this.expo[i], totalexpo);
+            if (ti >= 0) {
+                coeffiFinal[ti] = this.coeffi[i];
+            }
+        }
+
+        // Adding Pt 2
+        for (int i = 0; i < poly.expo.length; i++) {
+            int ti = indexChecker(poly.expo[i], totalexpo);
+            if (ti >= 0) {
+                coeffiFinal[ti] = coeffiFinal[ti] + poly.coeffi[i];
+            }
+        }
+
+        return new Polynomial(coeffiFinal, totalexpo);
+    }
+
+    // checks if expoenents are the same
+    private boolean dupChecker(int x, int[] expo) {
+        for (int i = 0; i < expo.length; i++) {
+            if (expo[i] == x) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private int indexChecker(int x, int[] expo) {
+        for (int i = 0; i < expo.length; i++) {
+            if (expo[i] == x) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    // Evaluate method
+    public double evaluate(double x) {
         double returnVal = 0;
-        double scalingX = 1;
-
-        for(double item: this.coeffi)
-        {
-            returnVal += scalingX * item;
-            scalingX = scalingX * x;
+        for (int i = 0; i < this.coeffi.length; i++) {
+            returnVal = returnVal + this.coeffi[i] * Math.pow(x, this.expo[i]);
         }
         return returnVal;
     }
 
-    //hasRoot Method
-    public boolean hasRoot(double x){
+    // hasRoot Method
+    public boolean hasRoot(double x) {
         return evaluate(x) == 0;
     }
 
-}
+    // Multiply Method
+    public Polynomial multiply(Polynomial poly) {
+        int maxLen = this.expo.length * poly.expo.length;
+        int[] temp = new int[maxLen];
 
+        for (int i = 0; i < maxLen; i++) {
+            temp[i] = -1;
+        }
+
+        int count = 0;
+
+        // For exponents
+        for (int i = 0; i < this.expo.length; i++) {
+            for (int j = 0; j < poly.expo.length; j++) {
+                if (!dupChecker(this.expo[i] + poly.expo[j], temp)) {
+                    temp[count] = this.expo[i] + poly.expo[j];
+                    count += 1;
+                }
+            }
+        }
+
+        // Final ans exponents
+        int[] fExpo = new int[count];
+
+        for (int i = 0; i < count; i++) {
+            fExpo[i] = temp[i];
+        }
+        Arrays.sort(fExpo);
+
+        // Final Coefficient calculation
+
+        double[] fCoeffi = new double[fExpo.length];
+
+        for (int i = 0; i < this.expo.length; i++) {
+            for (int j = 0; j < poly.expo.length; j++) {
+                int ti = indexChecker(this.expo[i] + poly.expo[j], fExpo);
+                fCoeffi[ti] = fCoeffi[ti] + this.coeffi[i] * poly.expo[j];
+            }
+        }
+
+        return new Polynomial(fCoeffi, fExpo);
+    }
+
+    // Constructor for type File
+    public Polynomial(File file) throws Exception {
+        Scanner myScanner = new Scanner(file);
+        if (!myScanner.hasNextLine()) {
+            this.coeffi = null;
+            this.expo = null;
+        } else {
+            String line = myScanner.nextLine();
+            line = line.replace("-", "+-");
+
+            String[] poly_arr = line.split("\\+");
+            this.expo = new int[poly_arr.length];
+            this.coeffi = new double[poly_arr.length];
+
+            for (int i = 0; i < poly_arr.length; i++) {
+                String[] subArry = poly_arr[i].split("x");
+                coeffi[i] = Double.parseDouble(subArry[0]);
+                if (subArry.length > 1) {
+                    expo[i] = Integer.parseInt(subArry[1]);
+                } else {
+                    expo[i] = 0;
+                }
+            }
+        }
+        myScanner.close();
+    }
+
+    // saveToFile Method
+    public void saveToFile(String myFile) throws Exception {
+        // if(coeffi == null || expo == null || this.coeffi.length != this.expo.length)
+        // {
+        // return;
+        // }
+
+        String writeString = "";
+
+        for (int i = 0; i < this.coeffi.length; i++) {
+            writeString += coeffi[i];
+            if (expo[i] != 0) {
+                writeString += "x" + expo[i];
+            }
+            writeString += "+";
+        }
+
+        if (writeString.endsWith("+")) {
+            writeString = writeString.substring(0, writeString.length() - 1);
+        }
+
+        FileWriter myWriter = new FileWriter(new File(myFile));
+        myWriter.write(writeString);
+        myWriter.close();
+
+    }
+}
